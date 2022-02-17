cb6185363b386cf0b8e3a46eec126b60c5fa2489
Aveek <aveeek.d22@gmail.com>
2022-02-17 23:55:51 +0100

    Fetch WP Posts

---
+++ app/wordpress.py
@@ -0,0 +1,40 @@
+import requests
+from config import Config
+
+WORDPRESS_URL = 'https://public-api.wordpress.com/'
+AUTH_TOKEN_URL = f'{WORDPRESS_URL}oauth2/token'
+
+SITE_ID = 167697861
+POST_ID = 142
+POSTS_URL = f'{WORDPRESS_URL}rest/v1.1/sites/{SITE_ID}/posts/{POST_ID}'
+
+
+auth_message_body = {
+    'client_id' : Config.CLIENT_ID,
+    'client_secret' : Config.CLIENT_SECRET,
+    'grant_type' : Config.GRANT_TYPE,
+    'username' : Config.USERNAME,
+    'password' : Config.APP_PASSWORD
+}
+
+content = requests.post(AUTH_TOKEN_URL, data=auth_message_body)
+
+response = content.json()
+
+ACCESS_TOKEN = response['access_token']
+
+
+headers = {
+    'Authorization' : f'Bearer {ACCESS_TOKEN}'
+}
+
+posts = requests.get(POSTS_URL, headers=headers)
+
+response = posts.json()
+
+print(response)
+
+
+
+
+
