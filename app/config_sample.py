4a0c3437b37d051707b99a2256462fca1088a691
Aveek <aveeek.d22@gmail.com>
2022-02-24 10:42:02 +0100

    Added sample config file

---
+++ app/config_sample.py
@@ -0,0 +1,23 @@
+
+
+class Wordpress:
+    """ This class can be used to store WordPress configurations. """
+
+    CLIENT_ID = ''
+    CLIENT_SECRET = ''
+    APP_PASSWORD = ''
+    USERNAME = ''
+    GRANT_TYPE = ''
+
+    SITE_ID = ''
+    POST_ID = ''
+
+
+class Twitter:
+    """ This class stores credentials for Twitter. """
+
+    CONSUMER_KEY        = ""
+    CONSUMER_KEY_SECRET = ""
+    ACCESS_TOKEN        = ""
+    ACCESS_TOKEN_SECRET = ""
+    BEARER_TOKEN        = ""
