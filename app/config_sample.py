49cca7a4669ff7563f9fb77dca5481928cac657a
Aveek <aveeek.d22@gmail.com>
2022-02-24 13:30:21 +0100

    Updated config sample.

--- app/config_sample.py
+++ app/config_sample.py
@@ -3,14 +3,14 @@
 class Wordpress:
     """ This class can be used to store WordPress configurations. """
 
-    CLIENT_ID = ''
-    CLIENT_SECRET = ''
-    APP_PASSWORD = ''
-    USERNAME = ''
-    GRANT_TYPE = ''
+    CLIENT_ID = ""
+    CLIENT_SECRET = ""
+    APP_PASSWORD = ""
+    USERNAME = ""
+    GRANT_TYPE = ""
 
-    SITE_ID = ''
-    POST_ID = ''
+    SITE_ID = ""
+    POST_ID = ""
 
 
 class Twitter:
@@ -21,3 +21,15 @@ class Twitter:
     ACCESS_TOKEN        = ""
     ACCESS_TOKEN_SECRET = ""
     BEARER_TOKEN        = ""
+
+
+class Facebook:
+    """  This class is used for Facebook configurations. """
+
+    APP_ID = ""
+    APP_NAME = ""
+    APP_SECRET = ""
+    CLIENT_TOKEN = ""
+    ACCESS_TOKEN = ""
+    PAGE_ID = ""
+    USER_ID = ""
