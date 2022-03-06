cb991af2135adf513ee778015d03ffbb3460c1be
Aveek <aveeek.d22@gmail.com>
2022-03-06 10:08:36 +0100

    Moved files to sandbox

--- app/instagram.py
+++
@@ -1,8 +0,0 @@
-import requests
-from config import Instagram
-
-
-account_id = Instagram.BUSINESS_ACCOUNT_ID
-access_token = Instagram.ACCESS_TOKEN
-
-image_url = "https://i.imgur.com/R4zJRaX.jpeg"
