04932afc2d7b6207b3832a9ab701052a44d208ea
Aveek <aveeek.d22@gmail.com>
2022-03-09 11:25:13 +0100

    Added support for Facebook

---
+++ sandbox/facebook.py
@@ -0,0 +1,31 @@
+import requests
+from config import Facebook
+
+# https://medium.com/nerd-for-tech/automate-facebook-posts-with-python-and-facebook-graph-api-858a03d2b142
+# https://www.youtube.com/watch?v=tfjmW0LEb64
+
+page_id = Facebook.PAGE_ID
+access_token = Facebook.PAGE_ACCESS_TOKEN
+user_id = Facebook.USER_ID
+
+
+text = f"Solving the Gilded Rose Refactoring Kata by @[{user_id}]. 123"
+url = "https://datacloudmag.com/solving-the-gilded-rose-refactoring-kata/"
+hashtags = "#python #refactoring"
+
+
+post_content = f"{text} \n\n{hashtags}"
+
+
+
+post_url = f"https://graph.facebook.com/v13.0/{page_id}/feed"
+
+
+payload = {
+    'message': post_content,
+    'access_token': access_token,
+    'link': url
+}
+r = requests.post(post_url, params=payload)
+
+print(r.text)
