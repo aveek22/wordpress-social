46879206cc4830900b3a3ed977fedfe492a939c1
Aveek <aveeek.d22@gmail.com>
2022-03-17 16:52:37 +0100

    Added more tests.

--- tests/test_linkedin.py
+++ tests/test_linkedin.py
@@ -10,6 +10,20 @@ from app.lambda_functions.social_integration.linkedin.publish import PublishLink
 
 # Unit Tests Cases here
 
-def test_create_instance_publish_linkedin():
+def test_check_organization_id():
+    """Checks the organization_id generated."""
     li = PublishLinkedIn()
     assert li.organization_id == '79377523'
+
+
+def test_check_user_name():
+    """Checks the user name generated."""
+    li = PublishLinkedIn()
+    assert li.username == 'Aveek Das'
+
+
+def test_check_linkedin_user_id():
+    """Checks the LinkedIn userID generated."""
+    li = PublishLinkedIn()
+    assert li.linkedin_user_id == 'urn:li:person:G0CopwI-SP'
+
