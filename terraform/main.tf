c22577d05c8a42eb8a89e68e00671ba6746592e6
Aveek <aveeek.d22@gmail.com>
2022-03-18 10:16:40 +0100

    Added terraform state to remote s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,11 @@
 module "social_integration__linkedin" {
-    source = "../terraform/social_integration__linkedin"
+    source  = "../terraform/social_integration__linkedin"
+}
+
+terraform {
+  backend "s3" {
+      bucket    = "wordpress-social"
+      key       = "terraform-state/terraform.tfstate"
+      region    = "eu-west-1"
+  }
 }
