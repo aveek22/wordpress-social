ad0e8cbe9c42e57ea435f8f7a04af409c75f6a94
Aveek <aveek.d22@gmail.com>
2022-04-04 21:30:12 +0200

    Added response status and body.

--- app/lambda_functions/auth_handler/app.py
+++ app/lambda_functions/auth_handler/app.py
@@ -1,14 +1,18 @@
 import requests
+import json
 
 def main(event, context):
     print(f"Printing from main handler...")
 
-    print(f"{event}")
+    # Extract code and state from request
+    response_body = {
+        'code'  : event['queryStringParameters']['code'],
+        'state' : event['queryStringParameters']['state']
+    }
+
+    print(f"Printing from Lambda.")
 
     return {
-        "statusCode": 200,
-        "body": {"foo": "bar"},
-        "headers": {
-            "Content-Type": "application/json"
-        }
+        'statusCode': 200,
+        'body': json.dumps(response_body)
     }
