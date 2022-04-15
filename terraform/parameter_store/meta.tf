4c3a6e229b2837b0c8aab40fe53bf9897f322c2c
Aveek <aveek.d22@gmail.com>
2022-04-15 14:20:06 +0200

    Added parameters for Instagram

--- terraform/parameter_store/meta.tf
+++ terraform/parameter_store/meta.tf
@@ -9,3 +9,21 @@ resource "aws_ssm_parameter" "facebook_user_access_token" {
   type  = "String"
   value = "${var.facebook_user_access_token}"
 }
+
+resource "aws_ssm_parameter" "instagram_app_id" {
+  name  = "/social_integration/instagram/app_id"
+  type  = "String"
+  value = "${var.instagram_app_id}"
+}
+
+resource "aws_ssm_parameter" "instagram_app_secret" {
+  name  = "/social_integration/instagram/app_secret"
+  type  = "String"
+  value = "${var.instagram_app_secret}"
+}
+
+resource "aws_ssm_parameter" "instagram_business_account_id" {
+  name  = "/social_integration/instagram/business_account_id"
+  type  = "String"
+  value = "${var.instagram_business_account_id}"
+}
