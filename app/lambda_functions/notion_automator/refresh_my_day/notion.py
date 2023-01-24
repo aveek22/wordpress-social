38443c995c061a212636dd70ec46bfc675e123c3
Aveek Das <aveek.das@dpgmedia.be>
2023-01-24 18:37:39 +0100

    Minor changes

--- app/lambda_functions/notion_automator/refresh_my_day/notion.py
+++ app/lambda_functions/notion_automator/refresh_my_day/notion.py
@@ -1,20 +1,19 @@
 # Import system libraries
-import boto3            # To interact with the AWS resources
-import requests         # Use REST API calls to interact with services
-import logging          # Log messages for debugging
-import os               # Get environment variables
-
+import boto3  # To interact with the AWS resources
+import requests  # Use REST API calls to interact with services
+import logging  # Log messages for debugging
+import os  # Get environment variables
 
 # Setup Logger
 log = logging.getLogger(__name__)
 
+
 class NotionMyDay:
     """ Refresh MyDay Tasks. """
 
-
     def __init__(self):
         log.debug(f'Initiate NotionMyDay')
-        
+
         self.header = self._get_header()
         log.debug(f'Header Set: {self.header}')
 
@@ -23,22 +22,21 @@ class NotionMyDay:
 
         log.info(f'NotionMyDay object initiated.')
 
-
     def _get_header(self):
         """ Prepare the header object with Authorization as Bearer Token. """
-        
+
         # Get the access token from Parameter Store
         log.debug(f'Get access token from AWS parameter store.')
         access_token = self._get_access_token_from_parameter_store()
         log.debug(f'Access token fetched.')
 
         # Prepare the header object
-        if(access_token):
+        if (access_token):
             log.debug(f'Setting up the header information.')
             headers = {
-                'Authorization'     : f'Bearer {access_token}',
-                'Content-Type'      : 'application/json',
-                'Notion-Version'    : '2021-05-13'
+                'Authorization': f'Bearer {access_token}',
+                'Content-Type': 'application/json',
+                'Notion-Version': '2021-05-13'
             }
             log.info(f'Header information set.')
             return headers
@@ -46,23 +44,21 @@ class NotionMyDay:
             log.warn(f'Header information not set. Application may not work as expected.')
             return False
 
-
     def _get_filter_payload(self):
         """ Prepare the Filter payload. """
-        
+
         log.debug(f'Setting up the Filter Payload.')
         filter_payload = {
-            "filter" : {
-                "property": "Day", 
+            "filter": {
+                "property": "Day",
                 "checkbox": {
-                    "equals" : True
+                    "equals": True
                 }
             }
         }
         log.info(f'Filter Payload set.')
-        
-        return filter_payload
 
+        return filter_payload
 
     def _get_access_token_from_parameter_store(self):
         """ Get the Access Token from AWS Parameter store. """
@@ -77,8 +73,8 @@ class NotionMyDay:
         # Get the Parameter details from AWS
         try:
             response = client.get_parameter(
-                Name = "/notion_automator/datacloudmag/access_token",
-                WithDecryption = False
+                Name="/notion_automator/datacloudmag/access_token",
+                WithDecryption=False
             )
             log.info(f'Access token obtained from parameter store.')
             # Return the parameter value
@@ -86,7 +82,6 @@ class NotionMyDay:
         except Exception as e:
             log.error(f'Error fetching parameter from Parameter Store. {e}')
             return False
-
 
     def _clean_my_day_response(self, response_json):
         """ Clean the JSON response and return Task Name and ID. """
@@ -98,12 +93,12 @@ class NotionMyDay:
 
         try:
             for task in response_json:
-                task_title  = task['properties']['Name']['title'][0]['plain_text']
-                task_id     = task['id']
+                task_title = task['properties']['Name']['title'][0]['plain_text']
+                task_id = task['id']
 
                 task_detail = {
-                    "task_title"    : task_title,
-                    "task_id"       : task_id
+                    "task_title": task_title,
+                    "task_id": task_id
                 }
 
                 log.info(f"Adding task detail: {task_detail}")
@@ -115,66 +110,65 @@ class NotionMyDay:
 
         return list_my_day
 
-    
     def get_my_day(self, event):
         """ Get MyDay tasks. """
-        
-        log.info(f"Received event payload. {event}")
-        database_id     = event['database_id']
-        database_url    = f"https://api.notion.com/v1/databases/{database_id}/query"
-        my_day_tasks    = False
 
-        if(self.header):
+        log.info(f"Received event payload. {event}")
+        database_id = event['database_id']
+        database_url = f"https://api.notion.com/v1/databases/{database_id}/query"
+        my_day_tasks = False
+
+        if self.header:
             try:
                 log.debug(f'Fetching MyDay tasks.')
                 response = requests.post(
-                    url     = database_url, 
-                    headers = self.header, 
-                    json    = self.filter_payload
+                    url=database_url,
+                    headers=self.header,
+                    json=self.filter_payload
                 )
                 log.debug(f'Response Status Code: {response.status_code}')
 
                 response_json = response.json()
                 log.debug(f'Response Content: {response_json}')
-                
-                if(response.status_code == 200):
+
+                if response.status_code == 200:
                     log.info(f'Fetched MyDay from Notion successfully.')
                     my_day_tasks = self._clean_my_day_response(response_json['results'])
-                elif(response.status_code == 404):
-                    log.info(f'Unable to fetch MyDay from database. Database does not exist or no access provided to integration.')
+                elif response.status_code == 404:
+                    log.info(
+                        f'Unable to fetch MyDay from database. Database does not exist or no access provided to '
+                        f'integration.')
                 else:
                     log.warn(f'Unable to fetch tasks from MyDay. Run in debug mode to view detailed error.')
             except Exception as e:
                 log.error(f'Error in fetching content from Notion. Error: {e}')
-                
-                
+
         return my_day_tasks
 
-    
-    def refresh_my_day(self,my_day_tasks):
+    def refresh_my_day(self, my_day_tasks):
         """ Refresh MyDay tasks. """
-        
+
         # For each task, need to change the MyDay status
 
-        my_day_refresh_payload = {"properties": {"Day" : False}}
+        my_day_refresh_payload = {"properties": {"Day": False}}
 
         for task in my_day_tasks:
 
-            page_id     = task['task_id']
-            page_url    = f"https://api.notion.com/v1/pages/{page_id}"
+            page_id = task['task_id']
+            page_url = f"https://api.notion.com/v1/pages/{page_id}"
 
-            if(self.header):
+            if (self.header):
                 try:
                     log.debug(f'Updating MyDay task - {task}.')
                     response = requests.request(
                         "PATCH",
-                        url     = page_url, 
-                        headers = self.header, 
-                        json    = my_day_refresh_payload
+                        url=page_url,
+                        headers=self.header,
+                        json=my_day_refresh_payload
                     )
                     log.debug(f'Response Status Code: {response.status_code}')
-                    
-                    if(response.status_code == 200):
+
+                    if (response.status_code == 200):
                         log.info(f'Updated MyDay - {task}.')
                     else:
                         log.warn(f'Unable to update tasks from MyDay. Run in debug mode to view detailed error.')
--- app/lambda_functions/social_integration/instagram/publish.py
+++ app/lambda_functions/social_integration/instagram/publish.py
@@ -1,28 +1,27 @@
 # Import system libraries
 from enum import Flag
 import profile
-import boto3            # To interact with the AWS resources
-import requests         # Use REST API calls to interact with services
-import logging          # Log messages for debugging
-import os               # Get environment variables
-
+import boto3  # To interact with the AWS resources
+import requests  # Use REST API calls to interact with services
+import logging  # Log messages for debugging
+import os  # Get environment variables
 
 # Setup Logger
 log = logging.getLogger(__name__)
 
+
 class PublishInstagram:
     """ Publish content to Instagram page. """
 
-
     def __init__(self):
         log.debug(f'Initiate PublishInstagram')
-        
+
         ig_details = self._get_instagram_profile_details_from_parameter_store()
         log.debug(f'LinkedIn profile details fetched.')
-        
+
         base_api_url = f"https://graph.facebook.com/v13.0/{self.business_account_id}"
-        self.media_api_url          = f"{base_api_url}/media"
-        self.media_publish_api_url  = f"{base_api_url}/media_publish"
+        self.media_api_url = f"{base_api_url}/media"
+        self.media_publish_api_url = f"{base_api_url}/media_publish"
         log.debug(f'API URLs set: {self.media_publish_api_url}')
 
         self.access_token = self._get_access_token()
@@ -30,23 +29,22 @@ class PublishInstagram:
 
         log.info(f'PublishInstagram object initiated.')
 
-
     def _get_access_token(self):
         """ Prepare Access Token from short-lived User Access Token """
-        
+
         # Get the user access token from Parameter Store
         log.debug(f'Get user access token from AWS parameter store.')
         user_access_token = self._get_user_access_token_from_parameter_store()
         log.debug(f'User Access token fetched.')
 
         # Prepare the Access Token
-        if(user_access_token):
+        if user_access_token:
             log.debug(f'Preparing Access Token from short-lived User Access Token.')
             oauth_url = f"https://graph.facebook.com/v13.0/oauth/access_token"
             oauth_params = {
-                'grant_type' : 'fb_exchange_token',
-                'client_id' : self.app_id,
-                'client_secret' : self.app_secret,
+                'grant_type': 'fb_exchange_token',
+                'client_id': self.app_id,
+                'client_secret': self.app_secret,
                 'fb_exchange_token': user_access_token
             }
             try:
@@ -61,7 +59,6 @@ class PublishInstagram:
         else:
             log.warn(f'Access Token not set. Application will quit.')
             return False
-
 
     def _get_user_access_token_from_parameter_store(self):
         """ Get the User Access Token from AWS Parameter store. """
@@ -78,8 +75,8 @@ class PublishInstagram:
         # Get the Parameter details from AWS
         try:
             response = client.get_parameter(
-                Name = "/social_integration/facebook/user_access_token",
-                WithDecryption = False
+                Name="/social_integration/facebook/user_access_token",
+                WithDecryption=False
             )
             log.info(f'User Access token obtained from parameter store.')
             # Return the parameter value
@@ -87,7 +84,6 @@ class PublishInstagram:
         except Exception as e:
             log.error(f'Error fetching parameter from Parameter Store. {e}')
             return False
-
 
     def _get_instagram_profile_details_from_parameter_store(self):
         """ Get Instagram profile details from AWS Parameter store. """
@@ -105,19 +101,18 @@ class PublishInstagram:
         # Get the Parameter details from AWS
         try:
             response = client.get_parameters_by_path(
-                Path = "/social_integration/instagram",
-                WithDecryption = False
+                Path="/social_integration/instagram",
+                WithDecryption=False
             )
             log.info(f'Instagram profile details obtained from parameter store.')
-            
+
             # Extract the profile details and set values
             ig_details = self._set_instagram_profile_details(response['Parameters'])
         except Exception as e:
             log.error(f'Error fetching parameter from Parameter Store. {e}')
-            
+
         return ig_details
-        
-        
+
     def _set_instagram_profile_details(self, instagram_details):
         # Set values to object
         log.debug(f"Setting Instagram profile details...")
@@ -136,26 +131,25 @@ class PublishInstagram:
                 elif param['Name'] == f"{param_path}/business_account_id":
                     log.debug(f"Business Account ID set.")
                     self.business_account_id = param['Value']
-            
+
             ig_details = True
         except Exception as e:
             log.error(f"Error setting Instagram profile details. Error: {e}")
 
         return ig_details
 
-
     def _upload_media(self, payload):
         """Upload media to Instagram"""
 
         media_id = False
 
-        if(self.access_token):
+        if (self.access_token):
             try:
                 log.debug(f'Uploading media to Instagram.')
-                response_media = requests.post(url=self.media_api_url, params = payload)
+                response_media = requests.post(url=self.media_api_url, params=payload)
                 log.debug(f'Response Status Code: {response_media.status_code}')
-                
-                if(response_media.status_code == 200):
+
+                if (response_media.status_code == 200):
                     response_media_json = response_media.json()
                     media_id = response_media_json['id']
                     log.debug(f'Response Media ID: {media_id}')
@@ -166,54 +160,51 @@ class PublishInstagram:
 
         return media_id
 
-
     def _publish_media(self):
         """Publish media to Instagram"""
 
-        if(self.media_id):
+        if (self.media_id):
             try:
                 log.debug(f'Publishing media to Instagram.')
                 publish_media_payload = {
-                    'creation_id' : f"{self.media_id}",
+                    'creation_id': f"{self.media_id}",
                     'access_token': f'{self.access_token}'
                 }
-                response_media = requests.post(url=self.media_publish_api_url, params = publish_media_payload)
+                response_media = requests.post(url=self.media_publish_api_url, params=publish_media_payload)
                 log.debug(f'Response Status Code: {response_media.status_code}')
-                
-                if(response_media.status_code == 200):
+
+                if (response_media.status_code == 200):
                     log.info(f'Content shared on Instagram successfully.')
                 else:
                     log.warn(f'Unable to upload media to Instagram. Run in debug mode to view detailed error.')
             except Exception as e:
                 log.error(f'Error in uploading media to Instagram. Error: {e}')
 
-    
     def post_content(self, payload):
         """ Share the content on LinkedIn Page. """
-        
+
         log.debug(f"Uploading media to Instagram")
         self.media_id = self._upload_media(payload)
 
         log.debug(f"Publish media on Instagram.")
         self._publish_media()
 
-    
-    def get_payload(self,event):
+    def get_payload(self, event):
         """ Prepare the content payload to share. """
-        
-        if('instagram' in event):
+
+        if ('instagram' in event):
             """Generate payload only if data available for instagram"""
 
-            image_url   = event['instagram']['ig_image_url']
-            caption     = event['instagram']['ig_caption']
-            hashtags    = event['instagram']['ig_hashtags']
-            share_url   = event['instagram']['ig_share_url']
-            
+            image_url = event['instagram']['ig_image_url']
+            caption = event['instagram']['ig_caption']
+            hashtags = event['instagram']['ig_hashtags']
+            share_url = event['instagram']['ig_share_url']
+
             log.info(f'Preparing payload for Instagram.')
 
             payload = {
-                'image_url' : image_url,
-                'caption' : f"{caption} \n {hashtags} \n {share_url}",
+                'image_url': image_url,
+                'caption': f"{caption} \n {hashtags} \n {share_url}",
                 'access_token': f'{self.access_token}'
             }
             log.info(f'Payload prepared.')
